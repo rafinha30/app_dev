bfba17fbbdfaad2ff1a093d91ec6c32eb90dc2f4
rafinha30 <gomes.reis.ads@gmail.com>
2023-04-07 10:41:44 -0300

    Projeto_DevClub

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function trocaCor(cor){
+   let  circulo = document.querySelector(".circulo") 
+   circulo.style.background = cor
+}
+
+
+function trocaImagem(imagem){
+    let img = document.querySelector(".iPhone")
+    img.src = imagem
+}
